25d95519ba8d0d86ef039d92995fb056eba05208
Tim Thompson <timster23@protonmail.com>
2023-08-12 20:59:23 -0500

    fix: CheckDev was returning undefined

--- src/engine/dev/check-dev-mode.ts
+++ src/engine/dev/check-dev-mode.ts
@@ -1,5 +1,9 @@
-const DEV = process.env.NODE_ENV === "development";
+const DEV =
+  typeof process !== "undefined"
+    ? process.env.NODE_ENV === "development"
+    : false;
 
 export function CheckDev(callback: Function) {
   if (DEV) return callback;
+  return () => {};
 }
