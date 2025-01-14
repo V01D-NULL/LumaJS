59f17ab59ce055c2204b76abe7fecb5cd76df4f5
Mystical <timster23@protonmail.com>
2025-01-14 06:28:50 +0100

    chore: make initial hydration function easier to read

--- packages/luma-cli/bin/compile/static/framework.ts
+++ packages/luma-cli/bin/compile/static/framework.ts
@@ -7,14 +7,17 @@ interface Window {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log(
-    window.__LUMA_FRAMEWORK__.default.__LUMA_INTERNAL__.hydrateRoot(
-      document.querySelector(".LumaAppRoot"),
-      window.__LUMA_FRAMEWORK__.default.jsx(
-        window.__LUMA_ROOT__.default,
-        { props: window.__LUMA_SSR_PROPS__.props },
-        null
-      )
-    )
+  const Framework = window.__LUMA_FRAMEWORK__.default;
+  const {
+    jsx,
+    __LUMA_INTERNAL__: { hydrateRoot },
+  } = Framework;
+
+  const props = window.__LUMA_SSR_PROPS__;
+  const componentRoot = window.__LUMA_ROOT__.default;
+
+  hydrateRoot(
+    document.querySelector(".LumaAppRoot"),
+    jsx(componentRoot, props, null)
   );
 });
