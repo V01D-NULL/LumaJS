b74b2026712b9cc7f656532c4b13a1ed0a300d0f
tim <timster23@protonmail.com>
2025-08-24 19:51:47 +0200

    chore: remove counter from about page

--- app/demo/src/pages/about/About.page.tsx
+++ app/demo/src/pages/about/About.page.tsx
@@ -1,17 +1,3 @@
-import Counter from "src/components/Counter";
-
 export default function About() {
-  return (
-    <div>
-      about page :)
-      <br />
-      <Counter />
-    </div>
-  );
-}
-
-export async function getServerProps() {
-  return {
-    props: {},
-  };
+  return <div>about page :)</div>;
 }
